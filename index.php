4a0be201ef977132b66a9e8438fd1f4ff1d1c15f
SophCSodre <sophia.c.sodre@hotmail.com>
2023-02-08 22:17:17 -0300

    quero ir pra casa

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Cadastrar Funcionário</title>
+</head>
+<body>
+    <h1>Gerenciar Funcionarios</h1>
+    <form action= "crud.php" method= "post">
+        Nome: <input type="text" name="nome"><br>
+        E-mail: <input type="email" name="email"><br>
+        CPF: <input type="text" name="cpf"><br>
+        Telefone: <input type="tel" name="telefone"><br>
+
+        <button>Cadastrar</button>
+        <button>Alterar</button>
+        <button>Excluir</button>
+        <button>Pesquisar</button>
+    </form>
+</body>
+</html>
